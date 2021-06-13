89a9c78ce3b5031e9351058f3fa0e145f637823c
heswell <steve@heswell.com>
2021-06-13 09:23:10 +0100

    initial commit

--- src/List.js
+++ src/List.js
@@ -1,13 +1,26 @@
 import "./List.css";
 
-export const List = ({ data }) => {
-  return (
-    <div className="List">
-      {data.map((item) => (
-        <div className="ListItem" key={item}>
-          {item}
-        </div>
-      ))}
-    </div>
-  );
+export const List = ({ source }) => {
+  const renderListItems = (data, listItems = []) => {
+    let headerIdx = 0;
+    for (let item of data) {
+      if (item.listItems) {
+        listItems.push(
+          <div className="ListItem ListHeader" key={`header-${headerIdx++}`}>
+            {item.label}
+          </div>
+        );
+        listItems.push(...renderListItems(item.listItems));
+      } else {
+        listItems.push(
+          <div className="ListItem" key={item}>
+            {item}
+          </div>
+        );
+      }
+    }
+    return listItems;
+  };
+
+  return <div className="List">{renderListItems(source)}</div>;
 };
